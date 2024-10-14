6284d2e0d3c77aa95fb3be28ced1ffc279276268
BlueSetbyeol <setbyeol.c@gmail.com>
2024-10-14 15:24:41 +0200

    Small application of the node template.

--- index.js
+++
@@ -1,5 +0,0 @@
-const userInformation = require("./userInformation");
-console.log(
-  `Hi, my name is ${userInformation.name} and I'm ${userInformation.age} years old`
-);
-userInformation.displayPassions();
